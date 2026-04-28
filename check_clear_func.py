87eb30bdc539cf28ee3feeb23375cdb9faef0f56
chominwoocho88-cloud <chominwoo.cho88@gmail.com>
2026-04-28 19:03:39 +0900

    v46

---
+++ check_clear_func.py
@@ -0,0 +1,11 @@
+﻿import re
+with open("orca/state.py", "r", encoding="utf-8") as f:
+    content = f.read()
+
+# clear_clustering_data 함수 찾기
+match = re.search(r"def clear_clustering_data\(.*?\n(.*?)(?=\ndef |\Z)", content, re.DOTALL)
+if match:
+    func_body = match.group(0)
+    print(func_body[:2000])  # 처음 2000자
+else:
+    print("Function not found!")
